6f6428c46f9fe6b6cef198121ecdf8aef4caf67e
PeterKim89 <112027726+PeterKim89@users.noreply.github.com>
2022-09-28 15:15:43 -0400

    general RPS logic added

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,63 @@ var wins = 0;
 var losses = 0;
 var ties = 0;
 
+function rockPaperScissors(){
+    if (userInput == "R")
+    {
+        if (computerInput == "R")
+        {
+            ties++;
+        }
+        else if (computerInput == "P")
+        {
+            losses++;
+        }
+        else if (computerInput == "S")
+        {
+            wins++;
+        }
+        else 
+        {
+            console.log("You're stupid");
+        }
+    }
+
+    if (userInput == "P")
+    {
+        if (computerInput == "R")
+        {
+            wins++;
+        }
+        else if (computerInput == "P")
+        {
+            ties++;
+        }
+        else if (computerInput == "S")
+        {
+            losses++;
+        }
+        else 
+        {
+            console.log("You're stupid");
+        }
+    }
+    if (userInput == "S")
+    {
+        if (computerInput == "R")
+        {
+            losses++;
+        }
+        else if (computerInput == "P")
+        {
+            wins++;
+        }
+        else if (computerInput == "S")
+        {
+            ties++;
+        }
+        else 
+        {
+            console.log("You're stupid");
+        }
+    }
+}
