f0c06bac09f8a2bfff78bb625e65d21a2fa6ed4e
PeterKim89 <112027726+PeterKim89@users.noreply.github.com>
2022-09-28 15:57:16 -0400

    Finalised project

--- assets/script.js
+++ assets/script.js
@@ -6,24 +6,37 @@ var wins = 0;
 var losses = 0;
 var ties = 0;
 
+function computerRandom() {
+    var index = Math.floor(Math.random() * rps.length)
+    computerInput = rps[index]; 
+}
+
 function rockPaperScissors(){
     if (userInput == "R")
     {
         if (computerInput == "R")
         {
             ties++;
+            alert("You played " + userInput + " and the computer played " + computerInput);
+            alert("You Tied!");
         }
         else if (computerInput == "P")
         {
             losses++;
+            alert("You played " + userInput + " and the computer played " + computerInput);
+            alert("You Lost!");
+
         }
         else if (computerInput == "S")
         {
             wins++;
+            alert("You played " + userInput + " and the computer played " + computerInput);
+            alert("You Won!");
+
         }
         else 
         {
-            console.log("You're stupid");
+            console.log("Please choose between R, P, or S");
         }
     }
 
@@ -32,18 +45,26 @@ function rockPaperScissors(){
         if (computerInput == "R")
         {
             wins++;
+            alert("You played " + userInput + " and the computer played " + computerInput);
+            alert("You Won!");
+
         }
         else if (computerInput == "P")
         {
             ties++;
+            alert("You played " + userInput + " and the computer played " + computerInput);
+            alert("You Tied!");
+
         }
         else if (computerInput == "S")
         {
             losses++;
+            alert("You played " + userInput + " and the computer played " + computerInput);
+            alert("You Lost!");
         }
         else 
         {
-            console.log("You're stupid");
+            console.log("Please choose between R, P, or S");
         }
     }
 
@@ -52,29 +73,57 @@ function rockPaperScissors(){
         if (computerInput == "R")
         {
             losses++;
+            alert("You played " + userInput + " and the computer played " + computerInput);
+            alert("You Lost!");
         }
         else if (computerInput == "P")
         {
             wins++;
+            alert("You played " + userInput + " and the computer played " + computerInput);
+            alert("You Won!");
+
         }
         else if (computerInput == "S")
         {
             ties++;
+            alert("You played " + userInput + " and the computer played " + computerInput);
+            alert("You Tied!");
+
         }
         else 
         {
-            console.log("You're stupid");
+            console.log("Please choose between R, P, or S");
         }
     }
 }
 
-userInput = prompt("Please input one of the follow:\nR, P, or S");
+function alertScore()
+{
+    alert("Wins: " + wins +"\nLosses: " + losses + "\nTies: " + ties);
+}
+
+function playRPS() 
+{
+    userInput = prompt("Please input one of the follow:\nR, P, or S");
+    computerRandom();
+    rockPaperScissors();
+    alertScore();
+    var playAgain = confirm("Would you like to play again");
+    if (playAgain == true)
+    {
+        playRPS();
+    }
+    else
+    {
+        alert("Thanks for playing!");
+    }
+}
 
 // console.log(Math.floor(10.99999)); // 10
 // console.log(Math.random());
 // 0 -> 0.9999999999999999999999999999999999
 // console.log(Math.floor(Math.random()));
-var index = Math.floor(Math.random() * rps.length)
-computerInput = rps[index]; 
 // console.log("This is user input: " + userInput);
 // console.log("This is computer input: " + computerInput);
+
+playRPS();
